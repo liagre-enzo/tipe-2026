bfc5ac926ff02a41552736b0a3cdf3f033c8c992
enzo.liagre <enzo.liagre@gmail.com>
2025-03-29 22:15:57 +0100

    tipe

---
+++ partie_1/lecture_memoire/reading.h
@@ -0,0 +1,9 @@
+#ifndef __READING_H__
+#define __READING_H__
+#include "../libraries.h"
+
+
+void print_section_of_memory(int, int);
+void print_memory_map(char*);
+
+#endif
---
+++ partie_1/libraries.h
@@ -0,0 +1,12 @@
+#ifndef __LIBRARIES_H__
+#define __LIBRARIES_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <assert.h>
+
+#endif
